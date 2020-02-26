4830b5aef22591daf94b76a5cb346844a004bbc6
bvmensvoort <info@bvmensvoort.nl>
2020-02-26 21:16:32 +0100

    Add possibility to choose operation modes and padidng on block ciphers, add dynamic input
    Add option mode and padding to GUI (only for block ciphers)
    Add dynamic input using msg.mode etc input
    Updated help text

--- cryptojs/decrypt.js
+++ cryptojs/decrypt.js
@@ -7,28 +7,43 @@ module.exports = function (RED) {
 		var node = this;
 		node.algorithm = config.algorithm;
 		node.key = config.key;
-		node.mode = config.mode;
-		node.padding = config.padding;
 
-		var CryptoJSConfig = {
-			mode: CryptoJS.mode[node.mode],
-			padding: CryptoJS.pad[node.padding]
+		if (_isBlockCipher(node.algorithm)) {
+			node.mode = config.mode;
+			node.padding = config.padding;
 		}
 
 		node.on('input', function (msg) {
+			var algorithm = Object.is(msg.algorithm, undefined) ? node.algorithm : msg.algorithm;
+			var key = Object.is(msg.key, undefined) ? node.key : msg.key;
+			var mode = Object.is(msg.mode, undefined) ? node.mode : msg.mode;
+			var padding = Object.is(msg.padding, undefined) ? node.padding : msg.padding;		
+			var CryptoJSConfig = {};
+			
+			if (_isBlockCipher(node.algorithm)) {
+				CryptoJSConfig = Object.assign(CryptoJSConfig, {
+					mode: CryptoJS.mode[mode],
+					padding: CryptoJS.pad[padding]
+				});
+			}
+
+			if (msg.drop) { CryptoJSConfig.drop = msg.drop; }
+			if (msg.iv) { CryptoJSConfig.iv = msg.iv; }
+			if (msg.salt) { CryptoJSConfig.iv = msg.salt; }
+
 			// check configurations
-			if(!node.algorithm || !node.key) {
+			if(!algorithm || !key) {
 				// rising misconfiguration error
-				node.error("Missing configuration, please check your algorithm or secret key.", msg);
+				node.error("Missing configuration, please check your algorithm, secret key, mode or padding.", msg);
 			} else {
 				// check the payload
 				if(msg.payload) {
 					// debugging message
-					node.debug('Encrypting payload using '+node.algorithm);
+					node.debug('Encrypting payload using '+ algorithm);
 					// decrypt with CryptoJS
-					let cipherMessage = CryptoJS.lib.CipherParams.create({ ciphertext: msg.payload });
-					let cipherKey = node.key;
-					msg.payload = CryptoJS[node.algorithm].decrypt(cipherMessage, cipherKey, CryptoJSConfig);
+					//let cipherMessage = CryptoJS.lib.CipherParams.create({ ciphertext: msg.payload });
+					var bytes = CryptoJS[algorithm].decrypt(msg.payload, key, CryptoJSConfig);
+					msg.payload = bytes.toString(CryptoJS.enc.Utf8);
 
 				} else {
 					// debugging message
@@ -38,6 +53,10 @@ module.exports = function (RED) {
 				node.send(msg);
 			}
 		});
+
+		function _isBlockCipher(algorithm) {
+			return (algorithm === "AES" || algorithm === "DES" || algorithm === "TripleDES");
+		}
 	}
 
 	RED.nodes.registerType("decrypt", DecryptNode);
